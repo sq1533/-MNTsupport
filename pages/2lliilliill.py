bc2db24e143a9ba4caf97c1deb261b3eb97b5708
sq1533 <618356@naver.com>
2024-02-27 17:32:14 +0900

    feat : 공지창 삭제

--- pages/2lliilliill.py
+++ pages/2lliilliill.py
@@ -11,15 +11,13 @@ st.markdown(css, unsafe_allow_html=True)
 #실시간 알람 불러오기
 with open('C:\\Users\\USER\\ve_1\\proj_web\\db\\info_.json','r',encoding="UTF-8") as f:
     DF = json.load(f)
-#네비게이터 버튼 'ilillili', 'lliilliill', 'iillilill', 'iillliiilll'
+#네비게이터 버튼 'ilillili', 'lliilliill', 'iillilill'
 row_ = row(4, vertical_align="top")
 if row_.button("모니터링", use_container_width=True):
     sp("IlIllIlI")
 row_.button("조회", use_container_width=True)
 if row_.button("DB관리", use_container_width=True):
     sp("IIllIlill")
-if row_.button("은행지연/모계좌", use_container_width=True):
-    sp("iilllIIIlll")
 
 mid = st.text_input("MID조회(입력 후 Enter)")
 look = [i for i in range(len(DF)) if DF[i]['mid']==mid]
--- pages/3iillilill.py
+++ pages/3iillilill.py
@@ -21,15 +21,13 @@ def change():
 def delete():
     requests.post(url_d,json.dumps(mk_d))
 
-#네비게이터 버튼 'ilillili', 'lliilliill', 'iillilill', 'iillliiilll'
+#네비게이터 버튼 'ilillili', 'lliilliill', 'iillilill'
 row_ = row(4, vertical_align="top")
 if row_.button("모니터링", use_container_width=True):
     sp("IlIllIlI")
 if row_.button("조회", use_container_width=True):
     sp("llIIllIIll")
 row_.button("DB관리", use_container_width=True)
-if row_.button("은행지연/모계좌", use_container_width=True):
-    sp("iilllIIIlll")
 
 tab1,tab2,tab3 = st.tabs(["생성","수정","삭제"])
 
